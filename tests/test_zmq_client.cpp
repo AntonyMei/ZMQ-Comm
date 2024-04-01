5c8a66017c43bceb6560145c1ea0c399e64c5fac
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-04-01 00:43:28 -0400

    + reformatted file

--- tests/test_zmq_client.cpp
+++ tests/test_zmq_client.cpp
@@ -11,25 +11,24 @@
 #include <string>
 #include <iostream>
 
-int main ()
-{
+int main() {
     //  Prepare our context and socket
-    zmq::context_t context (1);
-    zmq::socket_t socket (context, zmq::socket_type::req);
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, zmq::socket_type::req);
 
     std::cout << "Connecting to hello world server..." << std::endl;
-    socket.connect ("tcp://localhost:5555");
+    socket.connect("tcp://localhost:5555");
 
     //  Do 10 requests, waiting each time for a response
     for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq::message_t request (5);
-        memcpy (request.data (), "Hello", 5);
+        zmq::message_t request(5);
+        memcpy(request.data(), "Hello", 5);
         std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
-        socket.send (request, zmq::send_flags::none);
+        socket.send(request, zmq::send_flags::none);
 
         //  Get the reply.
         zmq::message_t reply;
-        socket.recv (reply, zmq::recv_flags::none);
+        socket.recv(reply, zmq::recv_flags::none);
         std::cout << "Received World " << request_nbr << std::endl;
     }
     return 0;
