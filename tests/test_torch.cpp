1c3d28110b046d37633bd151e564af246d935760
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-04-17 23:28:54 -0400

    + add test for pytorch

--- tests/test_torch.cpp
+++ tests/test_torch.cpp
@@ -8,6 +8,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
 
+#include "../src/utils.h"
+
 using namespace torch;
 namespace py = pybind11;
 
@@ -16,28 +18,29 @@ void call_python_function(const torch::Tensor& tensor) {
     py::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
     py::module_ module = py::module_::import("test_torch");
-    std::cout << "Successfully imported the module." << std::endl;
     auto proc_func = module.attr("print_tensor");
+    std::cout << "Python function successfully loaded." << std::endl;
 
     // convert the tensor
-    std::cout << "Converting tensor to python object." << std::endl;
+    // Calling takes 13 us
+    auto start = get_time();
     py::object py_tensor = py::cast(tensor);
-    std::cout << "Converted tensor to python object." << std::endl;
     py::object result = proc_func(py_tensor);
-
-    // get result
     auto result_tensor = result.cast<torch::Tensor>();
+    auto end = get_time();
+    std::cout << "Python function successfully called." << std::endl;
+    std::cout << "Calling time: " << end - start << " us\n";
 }
 
 
 int main() {
     // create a buffer
-    constexpr size_t buffer_size = 16 * 1024;
+    constexpr size_t buffer_size = 16;
     std::vector<char> buffer(buffer_size, 'a');
 
     // Create a CPU tensor from the buffer
     auto options = torch::TensorOptions().dtype(torch::kFloat16);
-    torch::Tensor tensor = torch::from_blob(buffer.data(), buffer.size(), options);
+    torch::Tensor tensor = torch::from_blob(buffer.data(), (int)buffer.size() / 2, options);
     std::cout << "Tensor successfully built on cpp side." << std::endl;
 
     // call the python function
