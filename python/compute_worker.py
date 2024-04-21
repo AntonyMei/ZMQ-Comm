2e645e2e29f582edb25db4428512bb8558f1e9aa
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-04-20 23:46:26 -0400

    + add python example code

--- python/compute_worker.py
+++ python/compute_worker.py
@@ -12,26 +12,57 @@ def main():
     worker_ip: str = utils.get_local_ip()
     assert worker_ip.startswith("10"), "Local IP must start with 10"
     llm_worker.start_network_threads(utils.CONFIG_BROADCAST_ADDR, worker_ip, "maxflow")
-    print("[Python] Cluster initialization finished!")
+    start_idx, end_idx, is_last_layer = llm_worker.get_model_start_end_idx()
+    print(f"[Python] Cluster initialization finished!")
+    print(f"[Python] Model layers: [{start_idx}, {end_idx}).")
+    print(f"[Python] Does this node output the last layer: {is_last_layer}.")
 
-    # TODO: python side main loop
     while True:
-        time.sleep(5)
-
-        # # fetch new requests to compute on
-        # start_time = time.time()
-        # request_ids, is_prompt, start_layer_idx, end_layer_idx, num_tokens, max_tokens, tensors = llm_sys.worker_fetch_new_requests()
-        # print("Time to fetch new requests: ", time.time() - start_time)
-        #
-        # # dummy compute
-        # result_tensor = torch.cat(tensors, dim=0)
-        # request_ids.sort()
-        # start_idx = [8192 * 1000 * i for i in request_ids]
-        # lengths = [8192 * 1000 for _ in request_ids]
-        # start_time = time.time()
-        # llm_sys.worker_submit_requests(request_ids, start_idx, lengths, result_tensor)
-        # print("Time to submit results: ", time.time() - start_time)
-        # time.sleep(20)
+        # ------------------------------------------------------------------------------------------- #
+        # Step 1: fetch new requests to compute on
+        start_time = time.time()
+        request_ids, is_prompt, start_layer_idx, end_layer_idx, num_tokens, max_tokens, tensors = llm_worker.fetch_new_requests()
+        if len(request_ids) == 0:
+            continue
+        print("Time to fetch new requests: ", time.time() - start_time)
+        print("Request ids: ", request_ids)
+        print("Is prompt: ", is_prompt)
+        print("Start layer idx: ", start_layer_idx)
+        print("End layer idx: ", end_layer_idx)
+        print("Num tokens: ", num_tokens)
+        print("Max tokens: ", max_tokens)
+        print("Tensor size: ", [tensor.size() for tensor in tensors])
+        print("Tensor dtype: ", [tensor.dtype for tensor in tensors])
+        print()
+        # ------------------------------------------------------------------------------------------- #
+        # Step 2: dummy compute (suppose activation size is 16 for each token)
+        if not is_last_layer:
+            cur_start_idx = 0
+            start_idx_list, length_list = [], []
+            for is_prompt, num_token in zip(is_prompt, num_tokens):
+                start_idx_list.append(cur_start_idx)
+                if is_prompt:
+                    length_list.append(num_token * 16)
+                    cur_start_idx += num_token * 16
+                else:
+                    length_list.append(1 * 16)
+                    cur_start_idx += 1 * 16
+            result_tensor = torch.arange(1, cur_start_idx + 1, dtype=torch.float16)  # a flattened tensor
+        else:
+            output_token_ids = [-1 if is_p else 1 for is_p in is_prompt]
+            start_idx_list = [i for i in range(len(request_ids))]
+            length_list = [1 for _ in range(len(request_ids))]
+            result_tensor = torch.tensor(output_token_ids, dtype=torch.int32)
+        print("Result tensor size: ", result_tensor.size())
+        print("Result tensor dtype: ", result_tensor.dtype)
+        print("Start idx list: ", start_idx_list)
+        print("Length list: ", length_list)
+        # ------------------------------------------------------------------------------------------- #
+        # Step 3: submit results
+        start_time = time.time()
+        llm_worker.submit_requests(request_ids, start_idx_list, length_list, result_tensor)
+        print("Time to submit results: ", time.time() - start_time)
+        # ------------------------------------------------------------------------------------------- #
 
 
 if __name__ == '__main__':
--- python/host.py
+++ python/host.py
@@ -14,39 +14,49 @@ def main():
     llm_host.start_network_threads(utils.CONFIG_BROADCAST_ADDR, host_ip, "./config.txt", "maxflow")
     print("[Python] Cluster initialization finished!")
 
-    # TODO: finish the python side main loop
     while True:
-        # data = [1] * 750
-        # start = time.time()
-        # llm_host.launch_request(
-        #     "prompt",
-        #     1,
-        #     750,
-        #     2000,
-        #     data,
-        #     True,
-        #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        # )
-        # end = time.time()
-        # print(f"prompt delta={end - start}")
+        # ------------------------------------------------------------------------------------------- #
+        # Step 1: Launch a few requests
         start = time.time()
-        for _ in range(1000):
+        llm_host.launch_request(
+            "prompt",  # request_type
+            1,  # request_id
+            750,  # num_tokens
+            2000,  # max_num_tokens
+            [i for i in range(750)],  # token_ids
+            True,  # set_routing
+            [1, 3, 0],  # server_ids
+            [0, 2, -1],  # start_layer_ids
+            [2, 4, -1],  # end_layer_ids
+        )
+        end = time.time()
+        print(f"prompt delta={end - start}")
+        start = time.time()
+        for i in range(3):
             llm_host.launch_request(
-                "decode",
-                1,
-                750,
-                2000,
-                [1],
-                True,
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+                "decode",  # request_type
+                2 + i,  # request_id
+                750,  # num_tokens
+                2000,  # max_num_tokens
+                [1],  # token_ids
+                True,  # set_routing
+                [1, 3, 0],  # server_ids
+                [0, 2, -1],  # start_layer_ids
+                [2, 4, -1],  # end_layer_ids
             )
         end = time.time()
-        print(f"decode speed={1000 / (end - start)}")
-        time.sleep(5)
+        print(f"decode speed={3 / (end - start)}")
+        # ------------------------------------------------------------------------------------------- #
+        # Step 1: Gather finished requests
+        time.sleep(10)
+        start = time.time()
+        finished_request_ids, generated_token_ids = llm_host.gather_finished_requests()
+        end = time.time()
+        print(f"gather delta={end - start}")
+        print(f"finished_request_ids={finished_request_ids}")
+        print(f"generated_token_ids={generated_token_ids}")
+        # ------------------------------------------------------------------------------------------- #
+        time.sleep(3000)  # for testing
 
 
 if __name__ == '__main__':
